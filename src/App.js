0682ee43d749689c611060e9c7e565474aace852
bidlomorph <dr.grechka@gmail.com>
2021-08-30 16:55:17 +0700

    Ending on USESTATE 36:55

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@
 import TodoList from "./todo/TodoList";
 
 function App() {
-  const todos = [
+  let todos = [
     {id: 0, completed: false, title: 'Buy bread'},
     {id: 1, completed: false, title: 'Buy eggs'},
     {id: 2, completed: false, title: 'Buy milk'}
-
   ]
+  function toggleTodo(id) {
+    todos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+  }
+
   return (
     <div className="container">
-      <TodoList todos={todos}/>
+      <TodoList todos={todos} onToggle={toggleTodo}/>
     </div>
   );
 }
--- src/todo/TodoItem.js
+++ src/todo/TodoItem.js
@@ -1,15 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function TodoItem({todo, index}) {
+const styles = {
+    li: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '.5rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        marginBottom: '.5rem'
+    },
+    span: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    input: {
+        marginRight: '1rem'
+    }
+}
+
+function TodoItem({todo, index, onChange}) {
     return (
-        <li>{index}: {todo.title}</li>
+        <li style={styles.li}>
+        <span style={styles.span}>
+            <input type="checkbox" style={styles.input} onChange={() => {
+                onChange(todo.id)
+            }}/>
+            <p>{index}: {todo.title}</p>
+        </span>
+        <button className={'rm'}>&times;</button>
+        </li>
     )
 }
 
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired,
-    index: PropTypes.number.isRequired
+    index: PropTypes.number.isRequired,
+    onChange: PropTypes.func.isRequired
 }
 
 export default TodoItem
